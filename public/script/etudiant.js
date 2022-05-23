063fe5e1af1630d922bc178863f75f501de98ee1
Your Name <bachikhsoukaina@gmail.com>
2022-05-23 09:23:52 +0100

    add reservation files

--- public/script/etudiant.js
+++ public/script/etudiant.js
@@ -6,3 +6,18 @@ createNavs();
 
 createmploi();
 createFooter();
+
+
+const navelement = document.querySelector(".nav-list");
+
+navelement.innerHTML = `
+<li>
+<a href="http://localhost:1337/planinpt">Plan de L'INPT</a>
+</li>
+<li>
+ <img class="user" src="../public/imgs/user.png" srcset=""/>
+  <ul class="navbar-dropdown">
+    <li>
+      <button class="login">login</button>
+    </li>
+`;
